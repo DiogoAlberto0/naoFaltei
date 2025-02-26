f3f3e507e64f1d3ff0c8cf72e369b12dea22a1d8
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-02-26 08:02:00 -0300

    feat: config `PrismaAdapter` on `NextAuth`

--- auth.ts
+++ auth.ts
@@ -3,6 +3,11 @@ import NextAuth from "next-auth";
 //providers
 import Google from "next-auth/providers/google";
 
+// prisma adapter
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 });
