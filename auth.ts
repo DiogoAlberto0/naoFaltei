fe3f85084503d5a6209ed2cc7d543b50d38269ab
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-02-26 07:04:30 -0300

    feat: add `Google` provider to next-auth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 
+//providers
+import Google from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google],
 });
