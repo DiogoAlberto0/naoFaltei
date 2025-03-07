0e1c6313729d76dad405cfc69ba5030b52dec3c6
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-07 17:52:43 -0300

    feat: create an API route to signin

---
+++ src/app/api/v1/signin/route.ts
@@ -0,0 +1,31 @@
+import { signIn } from "@/auth";
+import { InputError } from "@/src/Errors/errors";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password)
+      throw new InputError({
+        message: "Campos obrigatórios faltando.",
+        action: "Informe nome, email e senha do usuário",
+        status_code: 400,
+      });
+
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+
+    const signin = await signIn("credentials", formData);
+
+    return NextResponse.json({
+      signin,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      error,
+    });
+  }
+};
