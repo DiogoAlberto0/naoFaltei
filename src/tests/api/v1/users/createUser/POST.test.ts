fbcfc7948fc0ebdf8c5605e705b4713224994d81
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-07 18:59:13 -0300

    test: create function to authenticate an user

--- src/tests/api/v1/users/createUser/POST.test.ts
+++ src/tests/api/v1/users/createUser/POST.test.ts
@@ -1,15 +1,34 @@
 import { describe, it, expect, beforeAll } from "vitest";
 
-import { prisma } from "@/prisma/prisma";
+import { IUserWithoutHash, prisma } from "@/prisma/prisma";
 
 import { userModel } from "@/src/models/user";
+import { signinForTest } from "../../signinForTest";
+
+let validManager: IUserWithoutHash;
+const managerEmail = "manager@email.com";
+const managerPass = "123456789Abc.";
+let cookies: string;
 
 beforeAll(async () => {
   await prisma.$queryRawUnsafe(
     `TRUNCATE TABLE "users", "establishments" RESTART IDENTITY CASCADE;`
   );
 
-  expect(await userModel.count()).toEqual(0);
+  validManager = await userModel.create({
+    email: managerEmail,
+    password: managerPass,
+    name: "manager",
+  });
+
+  const resp = await signinForTest({
+    email: managerEmail,
+    password: managerPass,
+  });
+
+  cookies = resp.cookies;
+
+  expect(await userModel.count()).toEqual(1);
 });
 
 const validUser = {
@@ -20,8 +39,8 @@ const validUser = {
 
 describe("POST on /api/v1/user/createUser", () => {
   describe("Anonymous user", () => {
-    it("should not be possible to create a new user if name is not provided", async () => {
-      const response = await fetch(
+    it("asdfasdfasd", async () => {
+      const response2 = await fetch(
         "http://localhost:3000/api/v1/user/createUser",
         {
           method: "POST",
@@ -29,171 +48,189 @@ describe("POST on /api/v1/user/createUser", () => {
             ...validUser,
             name: "",
           }),
+          credentials: "include",
+          headers: {
+            Cookie: cookies,
+          },
         }
       );
 
-      expect(response.status).toEqual(400);
-
-      const json = await response.json();
-
-      expect(json).toStrictEqual({
-        message: "Campos obrigatórios faltando.",
-        action: "Informe nome, email e senha do usuário",
-      });
-
-      expect(await userModel.count()).toEqual(0);
+      console.log(await response2.json());
     });
+    // it("should not be possible to create a new user if name is not provided", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         ...validUser,
+    //         name: "",
+    //       }),
+    //     }
+    //   );
 
-    it("should not be possible to create a new user with invalid email", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            ...validUser,
-            email: "test-teste.com",
-          }),
-        }
-      );
+    //   expect(response.status).toEqual(400);
 
-      expect(response.status).toEqual(400);
+    //   const json = await response.json();
 
-      const json = await response.json();
+    //   expect(json).toStrictEqual({
+    //     message: "Campos obrigatórios faltando.",
+    //     action: "Informe nome, email e senha do usuário",
+    //   });
 
-      expect(json).toStrictEqual({
-        message: "Email inválido.",
-        action:
-          "Informe um email válido seguindo a seguinte estrutura: XXXX@XXXX.XXX",
-      });
+    //   expect(await userModel.count()).toEqual(0);
+    // });
 
-      expect(await userModel.count()).toEqual(0);
-    });
+    // it("should not be possible to create a new user with invalid email", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         ...validUser,
+    //         email: "test-teste.com",
+    //       }),
+    //     }
+    //   );
 
-    it("should not be possible to create a new user if email is not provided", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            ...validUser,
-            email: "",
-          }),
-        }
-      );
+    //   expect(response.status).toEqual(400);
 
-      expect(response.status).toEqual(400);
+    //   const json = await response.json();
 
-      const json = await response.json();
+    //   expect(json).toStrictEqual({
+    //     message: "Email inválido.",
+    //     action:
+    //       "Informe um email válido seguindo a seguinte estrutura: XXXX@XXXX.XXX",
+    //   });
 
-      expect(json).toStrictEqual({
-        message: "Campos obrigatórios faltando.",
-        action: "Informe nome, email e senha do usuário",
-      });
+    //   expect(await userModel.count()).toEqual(0);
+    // });
 
-      expect(await userModel.count()).toEqual(0);
-    });
+    // it("should not be possible to create a new user if email is not provided", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         ...validUser,
+    //         email: "",
+    //       }),
+    //     }
+    //   );
 
-    it("should not be possible to create a new user with invalid password", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            ...validUser,
-            password: "1234",
-          }),
-        }
-      );
+    //   expect(response.status).toEqual(400);
 
-      expect(response.status).toEqual(400);
+    //   const json = await response.json();
 
-      const json = await response.json();
+    //   expect(json).toStrictEqual({
+    //     message: "Campos obrigatórios faltando.",
+    //     action: "Informe nome, email e senha do usuário",
+    //   });
 
-      expect(json).toStrictEqual({
-        message: "Senha inválida.",
-        action:
-          "Informe uma senha válida, a senha deve conter ao menos uma letra maiúscula um número e um caracter especial.",
-      });
+    //   expect(await userModel.count()).toEqual(0);
+    // });
 
-      expect(await userModel.count()).toEqual(0);
-    });
+    // it("should not be possible to create a new user with invalid password", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         ...validUser,
+    //         password: "1234",
+    //       }),
+    //     }
+    //   );
 
-    it("should not be possible to create a new user if password is not provided", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            ...validUser,
-            password: "",
-          }),
-        }
-      );
+    //   expect(response.status).toEqual(400);
 
-      expect(response.status).toEqual(400);
+    //   const json = await response.json();
 
-      const json = await response.json();
+    //   expect(json).toStrictEqual({
+    //     message: "Senha inválida.",
+    //     action:
+    //       "Informe uma senha válida, a senha deve conter ao menos uma letra maiúscula um número e um caracter especial.",
+    //   });
 
-      expect(json).toStrictEqual({
-        message: "Campos obrigatórios faltando.",
-        action: "Informe nome, email e senha do usuário",
-      });
+    //   expect(await userModel.count()).toEqual(0);
+    // });
 
-      expect(await userModel.count()).toEqual(0);
-    });
+    // it("should not be possible to create a new user if password is not provided", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         ...validUser,
+    //         password: "",
+    //       }),
+    //     }
+    //   );
 
-    it("should be possible to create new user", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify(validUser),
-        }
-      );
+    //   expect(response.status).toEqual(400);
 
-      expect(response.status).toEqual(201);
+    //   const json = await response.json();
 
-      const json = await response.json();
+    //   expect(json).toStrictEqual({
+    //     message: "Campos obrigatórios faltando.",
+    //     action: "Informe nome, email e senha do usuário",
+    //   });
 
-      const userFromDB = await userModel.findByEmail(validUser.email);
+    //   expect(await userModel.count()).toEqual(0);
+    // });
 
-      expect(userFromDB).toEqual({
-        id: expect.any(String),
-        name: validUser.name,
-        email: validUser.email,
-        emailVerified: null,
-        image: null,
-        hash: expect.any(String),
-      });
+    // it("should be possible to create new user", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify(validUser),
+    //     }
+    //   );
 
-      expect(json).toStrictEqual({
-        id: expect.any(String),
-        name: validUser.name,
-        email: validUser.email,
-        emailVerified: null,
-        image: null,
-      });
-    });
+    //   expect(response.status).toEqual(201);
 
-    it("should not be possible to create a new user with an used email", async () => {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/user/createUser",
-        {
-          method: "POST",
-          body: JSON.stringify(validUser),
-        }
-      );
+    //   const json = await response.json();
 
-      expect(response.status).toEqual(409);
+    //   const userFromDB = await userModel.findByEmail(validUser.email);
 
-      const json = await response.json();
+    //   expect(userFromDB).toEqual({
+    //     id: expect.any(String),
+    //     name: validUser.name,
+    //     email: validUser.email,
+    //     emailVerified: null,
+    //     image: null,
+    //     hash: expect.any(String),
+    //   });
 
-      expect(json).toStrictEqual({
-        message: "O Email fornecido já está em uso por outro usuário.",
-        action: "Informe outro email",
-      });
+    //   expect(json).toStrictEqual({
+    //     id: expect.any(String),
+    //     name: validUser.name,
+    //     email: validUser.email,
+    //     emailVerified: null,
+    //     image: null,
+    //   });
+    // });
 
-      expect(await userModel.count()).toEqual(1);
-    });
+    // it("should not be possible to create a new user with an used email", async () => {
+    //   const response = await fetch(
+    //     "http://localhost:3000/api/v1/user/createUser",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify(validUser),
+    //     }
+    //   );
+
+    //   expect(response.status).toEqual(409);
+
+    //   const json = await response.json();
+
+    //   expect(json).toStrictEqual({
+    //     message: "O Email fornecido já está em uso por outro usuário.",
+    //     action: "Informe outro email",
+    //   });
+
+    //   expect(await userModel.count()).toEqual(1);
+    // });
   });
 });
