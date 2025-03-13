e776ede700c61c9bfbec7056d06779f133451cbc
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-13 07:25:02 -0300

    build: add component toast from heroui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "*",
-    "./node_modules/@heroui/theme/dist/components/(button|card|divider|form|input|toggle|ripple|spinner).js"
+    "./node_modules/@heroui/theme/dist/components/(button|card|divider|form|input|toggle|toast|ripple|spinner).js"
   ],
   theme: {
     extend: {},
