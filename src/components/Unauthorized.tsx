401cb7d21319a9b89a87791ca728ad53ef0ed168
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-14 09:22:21 -0300

    feat: create unauthorized page

---
+++ src/components/Unauthorized.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button } from "@heroui/button";
+import { Card, CardBody, CardHeader } from "@heroui/card";
+import { redirect } from "next/navigation";
+
+export const Unauthorized = () => {
+  return (
+    <main className="h-screen w-screen flex justify-center items-center bg-gradient-to-r from-red-500 to-yellow-500 p-10">
+      <Card className="w-full max-w-md p-6 bg-white shadow-lg rounded-lg">
+        <CardHeader className="pb-4 pt-2 px-4 text-center">
+          <h1 className="text-3xl font-semibold text-gray-700">
+            Acesso Negado
+          </h1>
+        </CardHeader>
+        <CardBody className="text-center">
+          <p className="text-lg text-gray-600 mb-4">
+            Você não tem permissão para acessar esta página.
+          </p>
+          <Button
+            color="danger"
+            size="lg"
+            onPress={() => {
+              redirect("/signin");
+            }}
+          >
+            Fazer Login
+          </Button>
+        </CardBody>
+      </Card>
+    </main>
+  );
+};
