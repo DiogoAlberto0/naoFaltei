62422ef319f005e7f081a5e3c2aa93f6aaae9b57
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-14 06:55:52 -0300

    feat: create a signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { GoogleSigninButton } from "@/src/components/GoogleSigninButton";
 import { Button, ButtonGroup } from "@heroui/button";
 export default function Home() {
   return (
-    <div className="bg-red-500">
+    <div className="bg-red-500 h-">
       <Button radius="sm" color="primary">
         Enviar
       </Button>
