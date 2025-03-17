4904b9762078ec1a35b7be5cb1217738ee7f0528
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-17 14:32:35 -0300

    feat: create component `EstablishmentsToggle`

--- src/components/EstablishmentsToggle/EstablishmentToggle.tsx
+++ src/components/EstablishmentsToggle/EstablishmentToggle.tsx
@@ -1,45 +1,100 @@
 "use client";
-import { ReactNode, useState } from "react";
-import { Button, Listbox, ListboxItem } from "@heroui/react";
+import { ReactNode, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+// hero ui components
+import { Button, Listbox, ListboxItem, useDisclosure } from "@heroui/react";
+
+// icons
 import { ArrowIcon } from "@/assets/icons/ArrowIcon";
 
-const ListboxItems = Array.from({ length: 30 }, (_, i) => i);
+// components
+import { AddEstablishmentFormModal } from "./AddEstablishmentFormModal";
+
+interface IEstablishmentProps {
+  id: string;
+  name: string;
+}
+
 export const ListboxWrapper = ({ children }: { children: ReactNode }) => (
-  <div className="w-full h-full max-h-full overflow-auto max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+  <div className="w-full h-full max-h-full overflow-hidden flex max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
     {children}
   </div>
 );
 export const EstablishmentToggle = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenToggle, setIsOpenToggle] = useState(true);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
+  const [establishments, setEstablishments] = useState<IEstablishmentProps[]>(
+    [],
+  );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchEstablishments = async () => {
+      const establishments = await fetch(
+        "http://localhost:3000/api/v1/establishment/list",
+      );
+
+      const data = await establishments.json();
+
+      setEstablishments(data.establishments);
+    };
+
+    fetchEstablishments();
+  }, []);
   return (
     <div className="relative flex h-full items-center">
-      {/* Botão para abrir/fechar */}
-
-      {/* Menu lateral */}
-
-      {isOpen && (
-        <ListboxWrapper>
-          <Listbox aria-label="Listbox menu with icons" variant="faded">
+      <ListboxWrapper>
+        {isOpenToggle && (
+          <Listbox
+            className="overflow-auto"
+            aria-label="Listbox menu with icons"
+            variant="faded"
+          >
             <>
-              {ListboxItems.map((i) => (
-                <ListboxItem key={i}>Estabelecimento {i}</ListboxItem>
+              {establishments.map(({ id, name }) => (
+                <ListboxItem
+                  onPress={() =>
+                    router.replace(`/manager/dashboard?establishmentId=${id}`, {
+                      scroll: false,
+                    })
+                  }
+                  key={id}
+                >
+                  {name}
+                </ListboxItem>
               ))}
             </>
-            <ListboxItem key="delete" className="text-success" color="success">
+            <ListboxItem
+              onPress={onOpen}
+              key="delete"
+              className="text-success"
+              color="success"
+            >
               Adicionar Estabelecimento
             </ListboxItem>
           </Listbox>
-        </ListboxWrapper>
-      )}
-
-      <Button color="primary" onPress={() => setIsOpen(!isOpen)} isIconOnly>
-        {isOpen ? (
-          <ArrowIcon direction="left" />
-        ) : (
-          <ArrowIcon direction="right" />
         )}
-      </Button>
+        <AddEstablishmentFormModal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+        />
+        <Button
+          color="primary"
+          variant="faded"
+          onPress={() => setIsOpenToggle(!isOpenToggle)}
+          isIconOnly
+          className="mx-1"
+        >
+          {isOpenToggle ? (
+            <ArrowIcon direction="left" />
+          ) : (
+            <ArrowIcon direction="right" />
+          )}
+        </Button>
+      </ListboxWrapper>
     </div>
   );
 };
