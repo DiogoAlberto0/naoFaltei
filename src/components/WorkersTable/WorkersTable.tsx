dbc0fca4dcdcce46bad66d357d92f4b353b14852
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-18 07:53:09 -0300

    feat: add `CreateWorkerModal` in `WorkersTable`

--- src/components/WorkersTable/WorkersTable.tsx
+++ src/components/WorkersTable/WorkersTable.tsx
@@ -18,6 +18,8 @@ import { AddIcon } from "@/assets/icons/AddIcon";
 import { DeleteIcon } from "@/assets/icons/DeleteIcon";
 import { EditIcon } from "@/assets/icons/EditIcon";
 import { EyeIcon } from "@/assets/icons/EyeIcon";
+import { useDisclosure } from "@heroui/modal";
+import { CreateWorkerModal } from "./CreateWorkerModal";
 
 const workers = [
   {
@@ -69,69 +71,81 @@ const ActionsToolTips = ({ id }: { id: string }) => (
   </div>
 );
 
-export const WorkersTable = () => {
+export interface IWorkersTableProps {
+  establishmentId: string;
+}
+export const WorkersTable = ({ establishmentId }: IWorkersTableProps) => {
+  const { onOpen, onOpenChange, isOpen } = useDisclosure();
   return (
-    <Table
-      aria-label="Example static collection table"
-      bottomContent={
-        <div className="flex w-full justify-center">
-          <Pagination
-            isCompact
-            showControls
-            showShadow
-            color="secondary"
-            page={1}
-            total={10}
-            onChange={(page) => console.log(page)}
-          />
-        </div>
-      }
-      classNames={{
-        base: "h-full w-full", // Ocupa 100% da altura e largura disponíveis
-        wrapper: "grow flex flex-col", // Permite que o wrapper cresça para preencher o espaço
-        table: "flex-1", // Faz a tabela ocupar o espaço disponível
-      }}
-      topContent={
-        <div className="flex justify-between items-center">
-          <h1 className="text-xl">Funcionários</h1>
-          <Button
-            size="sm"
-            isIconOnly
-            aria-label="Take a photo"
-            color="success"
-          >
-            <AddIcon className="stroke-current h-5 w-5" />
-          </Button>
-        </div>
-      }
-    >
-      <TableHeader>
-        <TableColumn>Funcionário</TableColumn>
-        <TableColumn>Status</TableColumn>
-        <TableColumn>Ações</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <>
-          {workers.map(({ id, email, name, isWorking }) => (
-            <TableRow key={id}>
-              <TableCell>
-                <div className="flex flex-col">
-                  <p className="text-bold text-sm capitalize">{name}</p>
-                  <p className="text-bold text-sm capitalize text-default-400">
-                    {email}
-                  </p>
-                </div>
-              </TableCell>
-              <TableCell>
-                <StatusChip isWorking={isWorking} />
-              </TableCell>
-              <TableCell>
-                <ActionsToolTips id={id} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </>
-      </TableBody>
-    </Table>
+    <>
+      <Table
+        aria-label="Example static collection table"
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color="secondary"
+              page={1}
+              total={10}
+              onChange={(page) => console.log(page)}
+            />
+          </div>
+        }
+        classNames={{
+          base: "h-full w-full", // Ocupa 100% da altura e largura disponíveis
+          wrapper: "grow flex flex-col", // Permite que o wrapper cresça para preencher o espaço
+          table: "flex-1", // Faz a tabela ocupar o espaço disponível
+        }}
+        topContent={
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Funcionários</h1>
+            <Button
+              size="sm"
+              isIconOnly
+              aria-label="Take a photo"
+              color="success"
+              onPress={onOpen}
+            >
+              <AddIcon className="stroke-current h-5 w-5" />
+            </Button>
+          </div>
+        }
+      >
+        <TableHeader>
+          <TableColumn>Funcionário</TableColumn>
+          <TableColumn>Status</TableColumn>
+          <TableColumn>Ações</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <>
+            {workers.map(({ id, email, name, isWorking }) => (
+              <TableRow key={id}>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <p className="text-bold text-sm capitalize">{name}</p>
+                    <p className="text-bold text-sm capitalize text-default-400">
+                      {email}
+                    </p>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <StatusChip isWorking={isWorking} />
+                </TableCell>
+                <TableCell>
+                  <ActionsToolTips id={id} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </>
+        </TableBody>
+      </Table>
+      <CreateWorkerModal
+        establishmentId={establishmentId}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      />
+    </>
   );
 };
