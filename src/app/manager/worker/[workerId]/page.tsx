68bfac7bc7e4ec2b148b6955164d50525fb57494
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-19 08:32:09 -0300

    feat: create page `/manager/worker/:workerId`

--- src/app/manager/worker/[workerId]/page.tsx
+++ src/app/manager/worker/[workerId]/page.tsx
@@ -1,12 +1,28 @@
+import { CalendarInput } from "@/src/components/CalendarInput/CalendarInput";
+import { RegistersTable } from "@/src/components/RegistersTable/RegistersTable";
+import { WorkerInfoCard } from "@/src/components/WorkerInfoCard/WorkerInfoCard";
+import { WorkSchedule } from "@/src/components/WorkSchedule/WorkSchedule";
+
 interface IWorkerPageProps {
   params: Promise<{ workerId: string }>;
 }
+
 const WorkerPage = async ({ params }: IWorkerPageProps) => {
   const { workerId } = await params;
   return (
-    <div className="w-full p-10 flex gap-1  bg-red-500">
-      <div className="flex-1 flex flex-col gap-1 bg-blue-500"></div>
-      <div className="flex-1 flex flex-col gap-1 bg-pink-500"></div>
+    <div className="w-full p-10 flex gap-1  ">
+      <div className="max-h-max w-50 flex flex-col gap-1 ">
+        <WorkerInfoCard />
+        <WorkSchedule />
+        <CalendarInput
+          title="Definir data:"
+          onSubmitRedirect="/manager/worker/1"
+          className="min-h-min"
+        />
+      </div>
+      <div className="flex-1 flex flex-col gap-1 ">
+        <RegistersTable title="20/03/2025 - 20/04/2025" maxRegisters={12} />
+      </div>
     </div>
   );
 };
