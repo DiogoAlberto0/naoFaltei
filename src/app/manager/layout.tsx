0eb997ba1701cfe69f056cbcc096b7a7dedba5f3
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-21 07:04:42 -0300

    style: modify pages to use modified EstablishmentToggle

--- src/app/manager/layout.tsx
+++ src/app/manager/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
 
       {/* Conteúdo principal */}
       <div className="flex-1 overflow-hidden">
-        <div className="h-full flex">
+        <div className="h-full max-sm:pb-12">
           <EstablishmentToggle />
           {children}
         </div>
--- src/app/manager/worker/[workerId]/page.tsx
+++ src/app/manager/worker/[workerId]/page.tsx
@@ -14,7 +14,7 @@ const WorkerPage = async (
 ) => {
   //const { workerId } = await params;
   return (
-    <div className="w-full p-5 sm:p-10 max-sm:p-0 max-sm:pl-10 flex gap-1 flex-wrap overflow-auto h-full max-h-full">
+    <div className="w-full p-5 sm:p-10 max-sm:p-0 flex gap-1 flex-wrap overflow-auto h-full max-h-full">
       <div className="flex-1 flex flex-col gap-1 md:max-h-full">
         <WorkerInfoCard />
         <WorkSchedule />
