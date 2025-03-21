1d50b1da92a1c9c33614e8c616815a48955df1f2
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-21 07:28:49 -0300

    refactor: make component `EstablishmentToggle` use `ToggleMenu` component

--- src/components/EstablishmentsToggle/EstablishmentToggle.tsx
+++ src/components/EstablishmentsToggle/EstablishmentToggle.tsx
@@ -1,16 +1,14 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 // hero ui components
 import { Listbox, ListboxItem, useDisclosure } from "@heroui/react";
 
-// icons
-import { ArrowIcon } from "@/assets/icons/ArrowIcon";
-
 // components
 import { AddEstablishmentFormModal } from "./AddEstablishmentFormModal";
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { ToggleMenu } from "../ToggleMenu/ToggleManu";
 
 interface IEstablishmentProps {
   id: string;
@@ -35,7 +33,6 @@ const renderItem = (
   );
 };
 export const EstablishmentToggle = () => {
-  const [isOpenToggle, setIsOpenToggle] = useState(true);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const [establishments, setEstablishments] = useState<IEstablishmentProps[]>(
@@ -62,53 +59,23 @@ export const EstablishmentToggle = () => {
     fetchEstablishments();
   }, []);
   return (
-    <div
-      className={`
-        border-small border-default-200 dark:border-default-100 rounded-r-small
-        bg-primary bg-opacity-50 hover:bg-opacity-90
-        absolute z-20 bottom-0
-        max-sm:w-full
-        flex flex-col-reverse
-        sm:h-[calc(100vh-4rem)] sm:flex-row
-        transition-all duration-500 ease-in-out 
-        ${isOpenToggle ? "h-[calc(100vh-4rem)] sm:w-[250px]" : "h-12 sm:w-12"}
-        `}
-    >
-      {isOpenToggle && (
-        <Listbox
-          className={`overflow-auto w-full h-full px-1 py-2 `}
-          aria-label="Listbox menu with icons"
-          variant="shadow"
-        >
-          <>{items}</>
-          <ListboxItem
-            onPress={onOpen}
-            key="delete"
-            className="text-success"
-            color="success"
-          >
-            Adicionar Estabelecimento
-          </ListboxItem>
-        </Listbox>
-      )}
-
-      <AddEstablishmentFormModal isOpen={isOpen} onOpenChange={onOpenChange} />
-      <button
-        onClick={() => setIsOpenToggle(!isOpenToggle)}
-        className=" flex justify-center items-center w-full h-12 sm:h-full sm:w-12"
+    <ToggleMenu>
+      <Listbox
+        className={`overflow-auto w-full h-full px-1 py-2 `}
+        aria-label="Listbox menu with icons"
+        variant="shadow"
       >
-        {isOpenToggle ? (
-          <>
-            <ArrowIcon direction="down" className="sm:hidden" />
-            <ArrowIcon direction="left" className="max-sm:hidden" />
-          </>
-        ) : (
-          <>
-            <ArrowIcon direction="top" className="sm:hidden" />
-            <ArrowIcon direction="right" className="max-sm:hidden" />
-          </>
-        )}
-      </button>
-    </div>
+        <>{items}</>
+        <ListboxItem
+          onPress={onOpen}
+          key="delete"
+          className="text-success"
+          color="success"
+        >
+          Adicionar Estabelecimento
+        </ListboxItem>
+      </Listbox>
+      <AddEstablishmentFormModal isOpen={isOpen} onOpenChange={onOpenChange} />
+    </ToggleMenu>
   );
 };
