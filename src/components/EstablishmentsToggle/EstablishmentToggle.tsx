83a7a4bf29a680de4aa2570d49034f1d52e3dc91
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-21 06:54:55 -0300

    style: make EstablishmentToggle more responsive

--- src/components/EstablishmentsToggle/EstablishmentToggle.tsx
+++ src/components/EstablishmentsToggle/EstablishmentToggle.tsx
@@ -10,12 +10,30 @@ import { ArrowIcon } from "@/assets/icons/ArrowIcon";
 
 // components
 import { AddEstablishmentFormModal } from "./AddEstablishmentFormModal";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 interface IEstablishmentProps {
   id: string;
   name: string;
 }
 
+const renderItem = (
+  { id, name }: IEstablishmentProps,
+  router: AppRouterInstance,
+) => {
+  return (
+    <ListboxItem
+      onPress={() =>
+        router.replace(`/manager/dashboard?establishmentId=${id}`, {
+          scroll: false,
+        })
+      }
+      key={id}
+    >
+      {name}
+    </ListboxItem>
+  );
+};
 export const EstablishmentToggle = () => {
   const [isOpenToggle, setIsOpenToggle] = useState(true);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -25,6 +43,10 @@ export const EstablishmentToggle = () => {
   );
 
   const router = useRouter();
+
+  const items = establishments.map((establishment) =>
+    renderItem(establishment, router),
+  );
 
   useEffect(() => {
     const fetchEstablishments = async () => {
@@ -40,63 +62,53 @@ export const EstablishmentToggle = () => {
     fetchEstablishments();
   }, []);
   return (
-    <div className="h-full relative ">
-      <div
-        className={`
-        border-small border-default-200 dark:border-default-100 
-        rounded-r-small
-        flex 
-        absolute z-50 sm:relative
-        h-full 
-        bg-content2 bg-opacity-90 
-        ${isOpenToggle && "w-screen sm:max-w-[250px]"}
+    <div
+      className={`
+        border-small border-default-200 dark:border-default-100 rounded-r-small
+        bg-primary bg-opacity-50 hover:bg-opacity-90
+        absolute z-20 bottom-0
+        max-sm:w-full
+        flex flex-col-reverse
+        sm:h-[calc(100vh-4rem)] sm:flex-row
+        transition-all duration-500 ease-in-out 
+        ${isOpenToggle ? "h-[calc(100vh-4rem)] sm:w-[250px]" : "h-12 sm:w-12"}
         `}
-      >
-        {isOpenToggle && (
-          <Listbox
-            className="overflow-auto w-full px-1 py-2"
-            aria-label="Listbox menu with icons"
-            variant="shadow"
-          >
-            <>
-              {establishments.map(({ id, name }) => (
-                <ListboxItem
-                  onPress={() =>
-                    router.replace(`/manager/dashboard?establishmentId=${id}`, {
-                      scroll: false,
-                    })
-                  }
-                  key={id}
-                >
-                  {name}
-                </ListboxItem>
-              ))}
-            </>
-            <ListboxItem
-              onPress={onOpen}
-              key="delete"
-              className="text-success"
-              color="success"
-            >
-              Adicionar Estabelecimento
-            </ListboxItem>
-          </Listbox>
-        )}
-        <AddEstablishmentFormModal
-          isOpen={isOpen}
-          onOpenChange={onOpenChange}
-        />
-        <button
-          onClick={() => setIsOpenToggle(!isOpenToggle)}
-          className=" flex justify-center items-center  w-10 "
+    >
+      {isOpenToggle && (
+        <Listbox
+          className={`overflow-auto w-full h-full px-1 py-2 `}
+          aria-label="Listbox menu with icons"
+          variant="shadow"
         >
-          {isOpenToggle ? (
-            <ArrowIcon direction="left" />
-          ) : (
-            <ArrowIcon direction="right" />
-          )}
-        </button>
-      </div>
+          <>{items}</>
+          <ListboxItem
+            onPress={onOpen}
+            key="delete"
+            className="text-success"
+            color="success"
+          >
+            Adicionar Estabelecimento
+          </ListboxItem>
+        </Listbox>
+      )}
+
+      <AddEstablishmentFormModal isOpen={isOpen} onOpenChange={onOpenChange} />
+      <button
+        onClick={() => setIsOpenToggle(!isOpenToggle)}
+        className=" flex justify-center items-center w-full h-12 sm:h-full sm:w-12"
+      >
+        {isOpenToggle ? (
+          <>
+            <ArrowIcon direction="down" className="sm:hidden" />
+            <ArrowIcon direction="left" className="max-sm:hidden" />
+          </>
+        ) : (
+          <>
+            <ArrowIcon direction="top" className="sm:hidden" />
+            <ArrowIcon direction="right" className="max-sm:hidden" />
+          </>
+        )}
+      </button>
     </div>
   );
 };
