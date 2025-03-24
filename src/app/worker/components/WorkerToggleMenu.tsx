af1cfc7f2f8d036f7e52da823bebe2e0c32726f7
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-24 07:36:45 -0300

    refactor: change `ListBoxItem` functionality from href to onPress change route

--- src/app/worker/components/WorkerToggleMenu.tsx
+++ src/app/worker/components/WorkerToggleMenu.tsx
@@ -3,24 +3,59 @@
 import { ToggleMenu } from "@/src/components/ToggleMenu/ToggleMenu";
 import { Listbox, ListboxItem } from "@heroui/react";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const WorkerToggleMenu = () => {
+  const router = useRouter();
+
+  const [selectedKeys, setSelectedKeys] = useState(new Set(["clockin"]));
+
   return (
     <ToggleMenu>
       <Listbox
-        className={`overflow-auto w-full h-full px-1 py-2 `}
+        className={`overflow-auto w-full h-full px-1 py-2`}
         aria-label="Listbox menu with icons"
         variant="shadow"
+        selectedKeys={selectedKeys}
+        selectionMode="single"
+        onSelectionChange={(keys) =>
+          setSelectedKeys(new Set(keys as Set<string>))
+        }
       >
-        <ListboxItem href="/worker" key="a" color="primary">
+        <ListboxItem
+          onPress={() => {
+            router.replace(`/worker`, {
+              scroll: false,
+            });
+          }}
+          key="clockin"
+          color="primary"
+        >
           Registrar Ponto
         </ListboxItem>
 
-        <ListboxItem href="/worker/timeSheet" key="b" color="primary">
+        <ListboxItem
+          onPress={() => {
+            router.replace(`/worker/timeSheet`, {
+              scroll: false,
+            });
+          }}
+          key="timeSheet"
+          color="primary"
+        >
           Folha de ponto
         </ListboxItem>
 
-        <ListboxItem href="/worker/updateInfos" key="c" color="primary">
+        <ListboxItem
+          onPress={() => {
+            router.replace(`/worker/updateInfos`, {
+              scroll: false,
+            });
+          }}
+          key="updateInfos"
+          color="primary"
+        >
           Alterar meus dados
         </ListboxItem>
 
