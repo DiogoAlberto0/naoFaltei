de34368cd30fb01ce8d03c9c85c4550afb20c382
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-24 18:50:41 -0300

    style: make page `/signin` more responsible

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,21 +9,15 @@ import { CredentialsSigninForm } from "@/src/components/CredentialsSigninForm/Cr
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-const leftSideSmStyles = `
-  max-md:w-full max-md:h-screen
-  max-md:absolute max-md:z-20 
-  max-md:bg-opacity-50
-`;
-const leftSideStyles = `
-  bg-background 
-  w-3/5 p-10 
-  flex gap-6 flex-col justify-center h-sm:justify-normal
-  overflow-auto
-  max-h-screen
-`;
-const LeftSide = ({ error }: { error: string | string[] | undefined }) => {
+const SigninForm = ({
+  error,
+  className,
+}: {
+  className?: string;
+  error: string | string[] | undefined;
+}) => {
   return (
-    <div className={`${leftSideStyles} ${leftSideSmStyles} `}>
+    <div className={className}>
       <h1 className="text-2xl font-bold">Faça login</h1>
       <Divider className="my-4" />
 
@@ -54,22 +48,6 @@ const LeftSide = ({ error }: { error: string | string[] | undefined }) => {
   );
 };
 
-const rightSideSmStyles = `
-  max-md:w-full 
-  max-md:relative max-md:z-10
-`;
-const rightSideStyles = `
-  h-full w-2/5 object-cover blur-sm
-`;
-const RightSide = () => {
-  return (
-    <Image
-      src={workersBackground}
-      alt="Background clock image"
-      className={`${rightSideSmStyles} ${rightSideStyles}`}
-    />
-  );
-};
 export default async function SigninPage({
   searchParams,
 }: {
@@ -79,11 +57,22 @@ export default async function SigninPage({
 
   return (
     <div className="h-screen flex relative">
-      <LeftSide error={error} />
+      <div className="w-3/5 flex justify-center items-center max-sm:absolute max-sm:w-full max-sm:h-full">
+        <SigninForm
+          className="max-sm:bg-content1 p-5 opacity-95"
+          error={error}
+        />
+      </div>
 
-      <RightSide />
+      <div className="bg-red-500 w-2/5 max-sm:w-full">
+        <Image
+          src={workersBackground}
+          alt=""
+          className="h-full w-full object-cover"
+        />
+      </div>
 
-      <ThemeSwitcher className="absolute bottom-6 right-6 z-30"></ThemeSwitcher>
+      <ThemeSwitcher className="absolute bottom-6 right-6 z-30" />
     </div>
   );
 }
