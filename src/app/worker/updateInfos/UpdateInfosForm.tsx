95e0567d84ec112a0149b4a468c1cc0d2a0d90a6
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-24 18:24:57 -0300

    feat: create component `UpdateInfosForm`

---
+++ src/app/worker/updateInfos/UpdateInfosForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { SaveButton } from "@/src/components/Buttons/SaveButton";
+import { PasswordInput } from "@/src/components/PasswordInput";
+import { Button, Form } from "@heroui/react";
+import { Input } from "@heroui/input";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+export const UpdateInfosForm = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const router = useRouter();
+
+  if (isClient)
+    return (
+      <Form
+        className="flex flex-col relative z-0 flex-1 justify-center"
+        onSubmit={(e) => console.log(e)}
+      >
+        <Input
+          name="email"
+          type="email"
+          required
+          label="Novo email:"
+          labelPlacement="outside"
+          placeholder="Digite o novo email aqui"
+        />
+        <PasswordInput
+          name="password"
+          required
+          label="Nova senha:"
+          labelPlacement="outside"
+          placeholder="Digite a nova senha aqui"
+        />
+
+        <PasswordInput
+          name="confirmPassword"
+          required
+          label="Confirme a senha:"
+          labelPlacement="outside"
+          placeholder="Digite a nova senha aqui"
+        />
+
+        <SaveButton className="w-full mt-5" />
+      </Form>
+    );
+};
