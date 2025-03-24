99a58c02d39236cdfd7b2b20e187a6ef4e14261a
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-24 19:43:26 -0300

    feat: use component `WorkerModal` on component `WorkersTable` to add a new worker

--- src/components/WorkersTable/WorkersTable.tsx
+++ src/components/WorkersTable/WorkersTable.tsx
@@ -1,11 +1,8 @@
 "use client";
 // next
-import { redirect } from "next/navigation";
+
 //heroui components
-import { Button } from "@heroui/button";
-import { Chip } from "@heroui/chip";
 import { Pagination } from "@heroui/pagination";
-import { Tooltip } from "@heroui/tooltip";
 import {
   Table,
   TableHeader,
@@ -15,13 +12,10 @@ import {
   TableCell,
 } from "@heroui/table";
 
-//Icons
-import { AddIcon } from "@/assets/icons/AddIcon";
-import { DeleteIcon } from "@/assets/icons/DeleteIcon";
-import { EditIcon } from "@/assets/icons/EditIcon";
-import { EyeIcon } from "@/assets/icons/EyeIcon";
-import { useDisclosure } from "@heroui/modal";
-import { CreateWorkerModal } from "./CreateWorkerModal";
+// Custom components
+import { WorkerModal } from "./WorkerModal";
+import { StatusChip } from "./WorkerStatusChip";
+import { ActionsToolTips } from "./WorkerActions";
 
 const workers = [
   {
@@ -42,64 +36,33 @@ const workers = [
   { id: "6", name: "Maria Souza", email: "maria@email.com", isWorking: true },
 ];
 
-const StatusChip = ({
-  isWorking,
-  responsive = false,
-}: {
-  isWorking: boolean;
-  responsive?: boolean;
-}) => (
-  <Chip
-    className="capitalize border-none gap-1 text-default-600"
-    color={isWorking ? "success" : "danger"}
-    size="sm"
-    variant="dot"
-  >
-    <p className={`${responsive && "hidden sm:flex"}`}>
-      {isWorking ? "Trabalhando" : "Ausente"}
-    </p>
-  </Chip>
-);
-
-const ActionsToolTips = ({ workerId }: { workerId: string }) => (
-  <div className="relative flex flex-col sm:flex-row items-center justify-center gap-2 ">
-    <Tooltip content="Details">
-      <span
-        onClick={() => redirect(`/manager/worker/${workerId}`)}
-        className="text-lg text-default-400 cursor-pointer active:opacity-50"
-      >
-        <EyeIcon />
-      </span>
-    </Tooltip>
-    <Tooltip content="Edit user">
-      <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-        <EditIcon />
-      </span>
-    </Tooltip>
-    <Tooltip color="danger" content="Delete user">
-      <span className="text-lg text-danger cursor-pointer active:opacity-50">
-        <DeleteIcon />
-      </span>
-    </Tooltip>
-  </div>
-);
-
 export interface IWorkersTableProps {
   establishmentId: string;
 }
+
+const Legend = () => {
+  return (
+    <div className="block sm:hidden">
+      <h2 className="text-sm">Legenda:</h2>
+      <StatusChip isWorking></StatusChip>
+      <StatusChip isWorking={false}></StatusChip>
+    </div>
+  );
+};
 export const WorkersTable = ({ establishmentId }: IWorkersTableProps) => {
-  const { onOpen, onOpenChange, isOpen } = useDisclosure();
   return (
     <>
       <Table
         aria-label="Example static collection table"
+        topContent={
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Funcionários</h1>
+            <WorkerModal establishmentId={establishmentId} type="create" />
+          </div>
+        }
         bottomContent={
           <div className="flex flex-col w-full justify-center ">
-            <div className="block sm:hidden">
-              <h2 className="text-sm">Legenda:</h2>
-              <StatusChip isWorking></StatusChip>
-              <StatusChip isWorking={false}></StatusChip>
-            </div>
+            <Legend />
             <div className="w-full flex justify-center items-center">
               <Pagination
                 size="sm"
@@ -119,21 +82,6 @@ export const WorkersTable = ({ establishmentId }: IWorkersTableProps) => {
           wrapper: "grow flex flex-col", // Permite que o wrapper cresça para preencher o espaço
           table: "flex-1 ", // Faz a tabela ocupar o espaço disponível
         }}
-        className="font"
-        topContent={
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl">Funcionários</h1>
-            <Button
-              size="sm"
-              isIconOnly
-              aria-label="Take a photo"
-              color="success"
-              onPress={onOpen}
-            >
-              <AddIcon className="stroke-current h-5 w-5" />
-            </Button>
-          </div>
-        }
       >
         <TableHeader>
           <TableColumn>Funcionário</TableColumn>
@@ -163,11 +111,6 @@ export const WorkersTable = ({ establishmentId }: IWorkersTableProps) => {
           </>
         </TableBody>
       </Table>
-      <CreateWorkerModal
-        establishmentId={establishmentId}
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-      />
     </>
   );
 };
