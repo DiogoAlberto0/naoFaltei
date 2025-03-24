4fe1075b9a92d8c56d3bd09dc34c54b18e0e4b7c
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-24 06:56:33 -0300

    feat: create main page to worker register clockin

--- src/app/worker/page.tsx
+++ src/app/worker/page.tsx
@@ -1,5 +1,18 @@
+// custom components
+import { RegisterMap } from "./components/RegisterMap";
+import { DateInfosHeader } from "./components/DateInfosHeader";
+
 const WorkerPage = () => {
-  return <h1>Worker</h1>;
+  return (
+    <main className="min-h-full max-h-full overflow-auto w-full relative flex flex-col">
+      <DateInfosHeader className="absolute z-10 left-0 top-0" />
+      <div className="flex-1 flex">
+        <RegisterMap />
+      </div>
+
+      <div className="bg-purple-500 h-10">Anuncio</div>
+    </main>
+  );
 };
 
 export default WorkerPage;
