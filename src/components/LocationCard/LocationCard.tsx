902255f728ac964a68ad0164f528f51b11539c62
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-25 07:58:53 -0300

    refactor: add CardProps and update component ´UpdateLocaleModal´

--- src/components/LocationCard/LocationCard.tsx
+++ src/components/LocationCard/LocationCard.tsx
@@ -1,33 +1,29 @@
 "use client";
-
-//icons
-import { EditIcon } from "@/assets/icons/EditIcon";
-
 //heroui components
-import { Button } from "@heroui/button";
-import { Card, CardFooter } from "@heroui/card";
-import { useDisclosure } from "@heroui/modal";
+import { Card, CardFooter, CardProps } from "@heroui/card";
 
 //components
 import { Map } from "@/src/components/Map/Map";
 import { UpdateLocaleModal } from "./UpdateLocaleModal";
 
-export const LocationCard = ({
-  markerPosition,
-  establishmentId,
-}: {
+interface ILocationCardProps extends CardProps {
   establishmentId: string;
   markerPosition: {
     lat: number;
     lng: number;
   };
-}) => {
-  const { isOpen, onOpenChange, onOpen } = useDisclosure();
-
+}
+export const LocationCard = ({
+  className,
+  markerPosition,
+  establishmentId,
+  ...otherProps
+}: ILocationCardProps) => {
   return (
     <Card
       isFooterBlurred
-      className="w-full h-[500px] col-span-12 sm:col-span-7"
+      className={`col-span-12 sm:col-span-7 ${className}`}
+      {...otherProps}
     >
       <Map
         className="z-0 w-full h-full object-cover"
@@ -38,21 +34,12 @@ export const LocationCard = ({
       />
       <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100 flex justify-between items-center">
         <h1 className="text-xl">Localização</h1>
-        <Button
-          endContent={<EditIcon className="h-5 w-5 stroke-primary-500" />}
-          color="primary"
-          onPress={onOpen}
-        >
-          Editar
-        </Button>
-      </CardFooter>
 
-      <UpdateLocaleModal
-        establishmentId={establishmentId}
-        inicialCoords={markerPosition}
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-      />
+        <UpdateLocaleModal
+          establishmentId={establishmentId}
+          inicialCoords={markerPosition}
+        />
+      </CardFooter>
     </Card>
   );
 };
