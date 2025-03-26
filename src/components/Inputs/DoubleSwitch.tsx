222b9c817d4d16bffe26c11bad82a503d089c7f2
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-26 05:49:15 -0300

    feat: create component `DoubleSwitch`

---
+++ src/components/Inputs/DoubleSwitch.tsx
@@ -0,0 +1,22 @@
+import { Switch, SwitchProps } from "@heroui/switch";
+
+interface IDoubleSwitchProps extends SwitchProps {
+  leftLabel: string;
+  rightLabel: string;
+}
+export const DoubleSwitch = ({
+  leftLabel,
+  rightLabel,
+  className,
+  ...otherProps
+}: IDoubleSwitchProps) => {
+  return (
+    <div
+      className={`flex items-center gap-3 bg-content2 p-2 rounded-lg ${className}`}
+    >
+      <span className="text-sm font-medium ">{leftLabel}</span>
+      <Switch name="Clockin" color="success" {...otherProps} />
+      <span className="text-sm font-medium ">{rightLabel}</span>
+    </div>
+  );
+};
