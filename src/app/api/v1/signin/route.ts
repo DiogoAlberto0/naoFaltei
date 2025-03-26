d1bb1a37be0a8aaa849b412acf9b8dd31554962a
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-26 17:43:02 -0300

    refactor: change the route `/signin` to recive a login field

--- src/app/api/v1/signin/route.ts
+++ src/app/api/v1/signin/route.ts
@@ -1,38 +1,39 @@
 import { signIn } from "@/auth";
 import { InputError, UnauthorizedError } from "@/src/Errors/errors";
-import { userModel } from "@/src/models/user";
+import { workerModel } from "@/src/models/worker";
 import { passwordUtils } from "@/src/utils/password";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
   try {
-    const { email, password } = await request.json();
+    const { login, password } = await request.json();
 
-    if (!email || !password)
+    if (!login || !password)
       throw new InputError({
         message: "Campos obrigatórios faltando.",
         action: "Informe nome, email e senha do usuário",
         status_code: 400,
       });
 
-    const existentUser = await userModel.findBy({
-      email,
+    const existentUser = await workerModel.findBy({
+      login,
     });
 
     if (!existentUser)
       throw new InputError({
-        message: "Usuário não encontrado",
-        action: "Contate o suporte",
+        message: "Usuário ou senha incorretos",
+        action: "Verifique os campos informados.",
       });
 
     if (!existentUser.hash) throw new UnauthorizedError();
 
     if (!passwordUtils.comparePassAndHash(password, existentUser.hash))
       throw new InputError({
-        message: "Senha incorreta",
+        message: "Usuário ou senha incorretos",
+        action: "Verifique os campos informados.",
       });
     const formData = new FormData();
-    formData.append("email", email);
+    formData.append("login", login);
     formData.append("password", password);
 
     await signIn("credentials", formData);
@@ -50,7 +51,7 @@ export const POST = async (request: NextRequest) => {
       },
       {
         status: error.status_code || 500,
-      }
+      },
     );
   }
 };
