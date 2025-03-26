b75a2a45162a68cb045ef40942ee8583b7ee5990
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-26 06:08:18 -0300

    style: make component 'RegisterClockModal' more responsible

--- src/app/manager/worker/[workerId]/RegisterClockModal.tsx
+++ src/app/manager/worker/[workerId]/RegisterClockModal.tsx
@@ -4,39 +4,37 @@ import { useState } from "react";
 //heroui
 import { Button } from "@heroui/button";
 import { useDisclosure } from "@heroui/modal";
-import { DatePicker, Switch } from "@heroui/react";
+import { DatePicker } from "@heroui/react";
 import { I18nProvider } from "@react-aria/i18n";
 import { now, getLocalTimeZone } from "@internationalized/date";
 
 // custom components
 import { AddIconButton } from "@/src/components/Buttons/AddIconButton";
 import { ModalForm } from "@/src/components/Modal/ModalForm";
+import { DeleteIconButton } from "@/src/components/Buttons/DeleteIconButton";
+import { DoubleSwitch } from "@/src/components/Inputs/DoubleSwitch";
 
 const ClockInputs = ({ onRemoveInput }: { onRemoveInput: () => void }) => {
   return (
-    <div className="flex justify-center items-center gap-5">
-      <I18nProvider locale="pt-br">
-        <DatePicker
-          hideTimeZone
-          showMonthAndYearPickers
-          name="dateTime"
-          defaultValue={now(getLocalTimeZone())}
-          label="Selecione a data e o horário:"
-          labelPlacement="outside"
-          isRequired
-          variant="faded"
-        />
-      </I18nProvider>
+    <div className="flex bg-content2 rounded-lg p-2 even:bg-content4 justify-center items-center gap-5">
+      <div className="flex-1 flex max-sm:flex-col justify-center items-center gap-5">
+        <I18nProvider locale="pt-br">
+          <DatePicker
+            hideTimeZone
+            showMonthAndYearPickers
+            name="dateTime"
+            defaultValue={now(getLocalTimeZone())}
+            label="Selecione a data e o horário:"
+            labelPlacement="outside"
+            isRequired
+            variant="faded"
+          />
+        </I18nProvider>
 
-      <div className="flex justify-center items-center gap-1 w-max">
-        <span>Entrada</span>
-        <Switch name="Clockin" color="success" />
-        <span>Saída</span>
+        <DoubleSwitch leftLabel="Entrada" rightLabel="Saída" />
       </div>
 
-      <Button color="danger" variant="flat" onPress={onRemoveInput}>
-        Remover
-      </Button>
+      <DeleteIconButton onPress={onRemoveInput} />
     </div>
   );
 };
@@ -59,6 +57,7 @@ export const RegisterClockModal = () => {
         }}
         onOpenChange={onOpenChange}
         isOpen={isOpen}
+        className=""
       >
         {inputsIds.map((id) => (
           <ClockInputs
@@ -71,7 +70,7 @@ export const RegisterClockModal = () => {
 
         <div className=" w-full flex justify-center items-center mt-4">
           <AddIconButton
-            color="default"
+            color="success"
             variant="flat"
             onPress={() => {
               setCounter((prevCounter) => prevCounter + 1);
