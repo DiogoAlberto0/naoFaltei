240fb4be1fb026566434cc09d569fa61b6e09ce5
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-03-26 18:35:16 -0300

    refactor: update user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,23 +1,24 @@
 import { prisma } from "@/prisma/prisma";
 
-// Errors
-import { ConflictError, InputError } from "../Errors/errors";
+// errors
+import { InputError } from "../Errors/errors";
+
 //utils
-import { emailUtils } from "../utils/email";
-import { passwordUtils } from "@/src/utils/password";
 import { cpfUtils } from "../utils/cpf";
-import { establishmentModel } from "./establishment";
+import { emailUtils } from "../utils/email";
 
 const findBy = async ({
   id,
   cpf,
   email,
   name,
+  login,
 }: {
   id?: string;
   email?: string;
   name?: string;
   cpf?: string;
+  login?: string;
 }) => {
   if (id) {
     return prisma.user.findUnique({
@@ -39,6 +40,10 @@ const findBy = async ({
     filters.push({ name });
   }
 
+  if (login) {
+    filters.push({ name });
+  }
+
   if (filters.length === 0) return null;
 
   return prisma.user.findFirst({
@@ -47,123 +52,8 @@ const findBy = async ({
     },
   });
 };
-
-const create = async ({
-  email,
-  password,
-  name,
-  cpf,
-  establishmentId,
-}: {
-  email: string;
-  password: string;
-  name: string;
-  cpf: string;
-  establishmentId: string;
-}) => {
-  if (!emailUtils.isValid(email))
-    throw new InputError({
-      message: "Email inválido.",
-      action:
-        "Informe um email válido seguindo a seguinte estrutura: XXXX@XXXX.XXX",
-      status_code: 400,
-    });
-
-  if (!passwordUtils.isValid(password))
-    throw new InputError({
-      message: "Senha inválida.",
-      action:
-        "Informe uma senha válida, a senha deve conter ao menos uma letra maiúscula um número e um caracter especial.",
-      status_code: 400,
-    });
-
-  if (!cpfUtils.isValid(cpf))
-    throw new InputError({
-      message: "CPF inválido",
-      action: "Informe um cpf válido",
-      status_code: 400,
-    });
-
-  const establishment = await establishmentModel.findBy({
-    id: establishmentId,
-  });
-
-  if (!establishment)
-    throw new InputError({
-      message: "Estabelecimento não encontrado",
-      action: "Informe o id de um estabelecimento válido",
-      status_code: 400,
-    });
-
-  const existentUser = await findBy({
-    cpf,
-    email,
-  });
-
-  if (!existentUser) {
-    const hashedPass = passwordUtils.genHash(password);
-
-    const createdUser = await prisma.user.create({
-      data: {
-        name,
-        email: emailUtils.normalize(email),
-        cpf: cpfUtils.clean(cpf),
-        hash: hashedPass,
-      },
-    });
-
-    await prisma.worker_on_establishments.create({
-      data: {
-        worker_id: createdUser.id,
-        establishment_id: establishment.id,
-      },
-    });
-    return {
-      id: createdUser.id,
-      name: createdUser.name,
-      email: createdUser.email,
-      emailVerified: createdUser.emailVerified,
-      image: createdUser.image,
-      cpf: createdUser.cpf,
-    };
-  } else {
-    const establishmentFromUser = await establishmentModel.listByWorker({
-      workerId: existentUser.id,
-    });
-
-    const isAlreadyAssociated = establishmentFromUser.some(
-      ({ id }) => establishment.id === id
-    );
-
-    if (isAlreadyAssociated)
-      throw new ConflictError({
-        message: "O usuário informado ja está associado ao estabelecimento",
-        action: "Verifique o usuário e o estabelecimento",
-      });
-    await prisma.worker_on_establishments.create({
-      data: {
-        establishment_id: establishment.id,
-        worker_id: existentUser.id,
-      },
-    });
-
-    return {
-      id: existentUser.id,
-      name: existentUser.name,
-      email: existentUser.email,
-      emailVerified: existentUser.emailVerified,
-      image: existentUser.image,
-      cpf: existentUser.cpf,
-    };
-  }
-};
-
-const count = async () => {
-  return await prisma.user.count();
-};
-
 const validateUser = async (userId: string) => {
-  const user = await userModel.findBy({ id: userId });
+  const user = await findBy({ id: userId });
 
   if (!user)
     throw new InputError({
@@ -172,5 +62,14 @@ const validateUser = async (userId: string) => {
       status_code: 400,
     });
 };
-const userModel = { create, findBy, count, validateUser };
+
+const count = async () => {
+  return await prisma.user.count();
+};
+const userModel = {
+  validateUser,
+  findBy,
+  count,
+};
+
 export { userModel };
