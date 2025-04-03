0be6ec48f97682bd556f994140edf3396b4f1609
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-04-03 06:03:28 -0300

    refactor: remove business rules from model

--- src/app/(back)/models/clockin.ts
+++ src/app/(back)/models/clockin.ts
@@ -1,72 +1,40 @@
 import { prisma } from "@/prisma/prisma";
-import { workerModel } from "./worker";
-import { establishmentModel } from "./establishment";
 
 const getLastRegisterToday = async (workerId: string) => {
   const today = new Date();
   return await prisma.clockin.findFirst({
     where: {
       worker: { id: workerId },
-      dateTime: {
+      date_time: {
         gte: new Date(today.setHours(0, 0, 0, 0)),
         lt: new Date(today.setHours(23, 59, 59, 999)),
       },
     },
     orderBy: {
-      dateTime: "desc",
+      date_time: "desc",
     },
   });
 };
 const register = async ({
   workerId,
   dateTime,
+  isEntry,
+  isTardiness,
+  lat,
+  lng,
 }: {
   dateTime: Date;
   workerId: string;
+  isEntry: boolean;
+  isTardiness: boolean;
   lat: string;
   lng: string;
 }) => {
-  const lastRegister = await getLastRegisterToday(workerId);
-  const schedule = await workerModel.getSchedule(workerId);
-  const localeEstablishment = await establishmentModel.getLocaleInfos({
-    workerId,
-  });
-
-  const isEntry = () => {
-    if (!lastRegister) return true;
-
-    return !lastRegister.is_entry;
-  };
-
-  const isTardiness = () => {
-    if (!schedule) return false; // Garante que `schedule` existe
-
-    const dayOfWeek = dateTime
-      .toLocaleDateString("en-US", { weekday: "long" })
-      .toLowerCase();
-    const scheduleOfDay = schedule[dayOfWeek];
-
-    if (!scheduleOfDay || scheduleOfDay.isDayOff) return false; // Verifica se há um horário para esse dia
-
-    // Se não há último registro, verificar se já passou do horário de entrada
-    if (!lastRegister) {
-      const currentHour = dateTime.getHours();
-      const currentMinute = dateTime.getMinutes();
-
-      const { startHour, startMinute } = scheduleOfDay; // Supondo que startMinute exista
-
-      if (currentHour > startHour) return true;
-      if (currentHour === startHour && currentMinute > startMinute) return true;
-    }
-
-    return false;
-  };
-
   await prisma.clockin.create({
     data: {
       date_time: dateTime,
-      is_entry: isEntry(),
-      is_tardiness: isTardiness(),
+      is_entry: isEntry,
+      is_tardiness: isTardiness,
       worker: { connect: { id: workerId } },
       registered_by: workerId,
       lat,
@@ -75,6 +43,6 @@ const register = async ({
   });
 };
 
-const clockinModel = { register };
+const clockinModel = { register, getLastRegisterToday };
 
 export { clockinModel };
