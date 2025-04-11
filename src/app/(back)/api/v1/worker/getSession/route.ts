da507ec14de6efacc9bd805fadeadc5d40295e80
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-04-11 09:20:09 -0300

    feat: add verification in route `/api/v1/worker/getSession`

--- src/app/(back)/api/v1/worker/getSession/route.ts
+++ src/app/(back)/api/v1/worker/getSession/route.ts
@@ -4,6 +4,8 @@ import { auth } from "@/auth";
 
 //errors
 import { UnauthorizedError } from "@/src/Errors/errors";
+import { workerModel } from "@/src/app/(back)/models/worker";
+import { userModel } from "@/src/app/(back)/models/user";
 
 export const GET = async (request: NextRequest) => {
   try {
@@ -12,6 +14,10 @@ export const GET = async (request: NextRequest) => {
     const session = await auth();
 
     if (session == null) throw new UnauthorizedError();
+    const worker = await workerModel.findUniqueBy({ id: session.user.id });
+    const author = await userModel.findBy({ id: session.user.id });
+
+    if (!worker || !author) throw new UnauthorizedError();
     return NextResponse.json({ session }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
