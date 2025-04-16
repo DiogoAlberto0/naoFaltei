47c31a54cba9cfff7e191672745c150976a923ee
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-04-16 06:24:21 -0300

    feat: integrate `RegisterClockModal` with backend

--- src/app/(front)/manager/worker/[workerId]/RegisterClockModal.tsx
+++ src/app/(front)/manager/worker/[workerId]/RegisterClockModal.tsx
@@ -3,82 +3,148 @@ import { useState } from "react";
 
 //heroui
 import { Button } from "@heroui/button";
-import { useDisclosure } from "@heroui/modal";
-import { DatePicker } from "@heroui/react";
+import { addToast, DatePicker, Input } from "@heroui/react";
 import { I18nProvider } from "@react-aria/i18n";
-import { now, getLocalTimeZone } from "@internationalized/date";
 
 // custom components
 import { AddIconButton } from "@/src/app/(front)/components/Buttons/AddIconButton";
 import { ModalForm } from "@/src/app/(front)/components/Modal/ModalForm";
 import { DeleteIconButton } from "@/src/app/(front)/components/Buttons/DeleteIconButton";
-import { DoubleSwitch } from "@/src/app/(front)/components/Inputs/DoubleSwitch";
+import { RemoveIconButton } from "../../../components/Buttons/RemoveIconButton";
 
-const ClockInputs = ({ onRemoveInput }: { onRemoveInput: () => void }) => {
+//errors
+import { InputError } from "@/src/Errors/errors";
+
+//fetcher
+import { axios } from "@/src/utils/fetcher";
+
+const registerClockinHandler = async (formData: FormData) => {
+  const workerId = formData.get("workerId");
+  if (!workerId)
+    throw new InputError({
+      message: "Falha ao identificar o usuário",
+      action: "Recarregue a página",
+    });
+  const days: { [key: string]: string } = {};
+  const timesPerDay: { [key: string]: string[] } = {};
+  const registers: { clockedAt: string }[] = [];
+
+  for (const [key, value] of formData.entries()) {
+    if (key.includes("day")) {
+      const [, id] = key.split("-");
+      days[id] = value.toString();
+    } else if (key.includes("time")) {
+      const [, id] = key.split("-");
+      timesPerDay[id] = [...(timesPerDay[id] || []), value.toString()];
+    }
+  }
+
+  for (const day in timesPerDay) {
+    for (const time of timesPerDay[day]) {
+      const localDate = new Date(`${days[day]}T${time}`);
+      registers.push({ clockedAt: localDate.toISOString() });
+    }
+  }
+
+  const { data } = await axios<{ message: string }>({
+    route: "/api/v1/clockin/managerRegister",
+    method: "POST",
+    body: {
+      workerId,
+      registers,
+    },
+  });
+
+  addToast({
+    color: "success",
+    title: "Folha de ponto atualizada com sucesso",
+    description: data.message,
+  });
+};
+const ClockInputs = ({
+  onRemoveInput,
+  clockId,
+}: {
+  onRemoveInput: () => void;
+  clockId: number;
+}) => {
+  const [counter, setCounter] = useState(1);
+  const [inputIds, setInputIds] = useState([0]);
+
   return (
     <div className="flex bg-content2 rounded-lg p-2 even:bg-content4 justify-center items-center gap-5">
-      <div className="flex-1 flex max-sm:flex-col justify-center items-center gap-5">
+      <div className="flex-1 flex flex-col max-sm:flex-col justify-center items-center gap-5">
         <I18nProvider locale="pt-br">
           <DatePicker
-            hideTimeZone
-            showMonthAndYearPickers
-            name="dateTime"
-            defaultValue={now(getLocalTimeZone())}
-            label="Selecione a data e o horário:"
-            labelPlacement="outside"
             isRequired
-            variant="faded"
+            label="Selecione a data:"
+            name={`day-${clockId}`}
           />
         </I18nProvider>
-
-        <DoubleSwitch leftLabel="Entrada" rightLabel="Saída" />
+        <div className=" flex-1 w-full flex flex-wrap gap-2 items-center">
+          {inputIds.map((id, index) => (
+            <Input
+              key={id}
+              id={id.toString()}
+              label={index % 2 == 0 ? "Entrada" : "Saída"}
+              variant={index % 2 == 0 ? "bordered" : "underlined"}
+              type="time"
+              className="w-max"
+              name={`time-${clockId}`}
+            />
+          ))}
+          <RemoveIconButton
+            onPress={() => setInputIds((prev) => prev.slice(0, -1))}
+          />
+          <AddIconButton
+            onPress={() => {
+              setCounter((prevState) => prevState + 1);
+              setInputIds((prevState) => [...prevState, counter]);
+            }}
+          />
+        </div>
       </div>
 
       <DeleteIconButton onPress={onRemoveInput} />
     </div>
   );
 };
-export const RegisterClockModal = () => {
-  const { isOpen, onOpenChange, onOpen } = useDisclosure();
-
+export const RegisterClockModal = ({ workerId }: { workerId: string }) => {
   const [counter, setCounter] = useState(1);
   const [inputsIds, setInputsIds] = useState<number[]>([0]);
 
   return (
-    <>
-      <Button color="primary" onPress={onOpen}>
-        Registrar Entradas/Saídas
-      </Button>
-      <ModalForm
-        title="Registrar entradas e saídas do funcionário"
-        submitButtonText="Registrar"
-        handleSubmit={async (formData) => {
-          console.log(formData.get("DateTime"));
-        }}
-        onOpenChange={onOpenChange}
-        isOpen={isOpen}
-        className=""
-      >
-        {inputsIds.map((id) => (
-          <ClockInputs
-            key={id}
-            onRemoveInput={() => {
-              setInputsIds((prev) => prev.filter((inputId) => inputId !== id));
-            }}
-          />
-        ))}
+    <ModalForm
+      title="Registrar entradas e saídas do funcionário"
+      submitButtonText="Registrar"
+      handleSubmit={registerClockinHandler}
+      openButton={({ onPress }) => (
+        <Button color="primary" onPress={onPress}>
+          Registrar Entradas/Saídas
+        </Button>
+      )}
+    >
+      <Input name="workerId" type="hidden" value={workerId} />
+      {inputsIds.map((id) => (
+        <ClockInputs
+          key={id}
+          clockId={id}
+          onRemoveInput={() => {
+            setInputsIds((prev) => prev.filter((inputId) => inputId !== id));
+          }}
+        />
+      ))}
 
-        <div className=" w-full flex justify-center items-center mt-4">
-          <AddIconButton
-            color="success"
-            variant="flat"
-            onPress={() => {
-              setCounter((prevCounter) => prevCounter + 1);
-              setInputsIds((prev) => [...prev, counter]);
-            }}
-          />
-        </div>
-      </ModalForm>
-    </>
+      <div className=" w-full flex justify-center items-center mt-4">
+        <AddIconButton
+          color="success"
+          variant="flat"
+          onPress={() => {
+            setCounter((prevCounter) => prevCounter + 1);
+            setInputsIds((prev) => [...prev, counter]);
+          }}
+        />
+      </div>
+    </ModalForm>
   );
 };
--- src/app/(front)/manager/worker/[workerId]/page.tsx
+++ src/app/(front)/manager/worker/[workerId]/page.tsx
@@ -1,10 +1,14 @@
+import { cookies } from "next/headers";
+
+//fetcher
+import { axios } from "@/src/utils/fetcher";
+
+//components
 import { CalendarInput } from "@/src/app/(front)/components/CalendarInput/CalendarInput";
 import { RegistersTable } from "@/src/app/(front)/components/RegistersTable/RegistersTable";
 import { WorkerInfoCard } from "@/src/app/(front)/components/WorkerInfoCard/WorkerInfoCard";
 import { WorkSchedule } from "@/src/app/(front)/components/WorkSchedule/WorkSchedule";
 import { RegisterClockModal } from "./RegisterClockModal";
-import { axios } from "@/src/utils/fetcher";
-import { cookies } from "next/headers";
 
 interface IWorkerPageProps {
   params: Promise<{ workerId: string }>;
@@ -22,11 +26,13 @@ export interface IWorker {
   is_active: true;
   establishment_id: string;
 }
+
 const WorkerPage = async ({ params }: IWorkerPageProps) => {
   const { workerId } = await params;
 
   const cookie = await cookies();
-  const { data } = await axios<IWorker>({
+
+  const { data: worker } = await axios<IWorker>({
     route: `/api/v1/worker/${workerId}/details`,
     cookie: cookie.toString(),
   });
@@ -34,9 +40,10 @@ const WorkerPage = async ({ params }: IWorkerPageProps) => {
   return (
     <div className="w-full h-full max-h-full overflow-auto p-4 sm:p-6 md:p-8 flex flex-col md:flex-row gap-4">
       <div className="flex-1 flex flex-col gap-4">
-        <WorkerInfoCard worker={data} />
-        {/* <WorkSchedule />
-        <RegisterClockModal /> */}
+        <WorkerInfoCard worker={worker} />
+        <WorkSchedule workerId={workerId} />
+
+        <RegisterClockModal workerId={workerId} />
       </div>
       <div className="flex-1 flex flex-col gap-4">
         {/* 
