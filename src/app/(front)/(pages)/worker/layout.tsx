28f23d5e4725263782928de787fc43ed006833b6
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-04-30 07:43:34 -0300

    feat: use component `AdBanner` on page `/worker`

--- src/app/(front)/(pages)/worker/layout.tsx
+++ src/app/(front)/(pages)/worker/layout.tsx
@@ -16,7 +16,7 @@ export default async function WorkerLayout({
   return (
     <div className="h-dvh w-dvw max-h-dvh flex flex-col overflow-hidden relative">
       <WorkerToggleMenu />
-      <div className="flex-1 h-full w-full max-w-full overflow-auto max-sm:pb-12 sm:pl-10">
+      <div className="flex-1 h-full w-full max-w-full overflow-auto max-sm:pb-14 sm:pl-10">
         {children}
       </div>
     </div>
