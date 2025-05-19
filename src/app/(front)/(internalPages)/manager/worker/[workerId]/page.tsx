7f952e0f4770a43ccbeaace43c890a467fed09f2
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-05-19 14:57:00 -0300

    feat: make demo version of page `/manager/worker/:workerId`

--- src/app/(front)/(internalPages)/manager/worker/[workerId]/page.tsx
+++ src/app/(front)/(internalPages)/manager/worker/[workerId]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 //components
 import { CalendarInput } from "@/src/app/(front)/components/Inputs/CalendarInput/CalendarInput";
 import { RegistersTable } from "@/src/app/(front)/components/Tables/RegistersTable/RegistersTable";
@@ -5,6 +6,7 @@ import { WorkerInfoCard } from "@/src/app/(front)/components/Cards/WorkerInfoCar
 import { WorkSchedule } from "@/src/app/(front)/components/Tables/WorkSchedule/WorkSchedule";
 import { RegisterClockModal } from "../../../../components/Modal/RegisterClockinModal/RegisterClockModal";
 import { RegisterMedicalLeaveModal } from "@/src/app/(front)/components/Modal/RegisterMedicalLeaveModal/RegisterMedicalLeave";
+import { Unauthorized } from "@/src/app/(front)/components/Unauthorized";
 
 // getter
 import { getWorkerDetails } from "@/src/app/(front)/hooks/worker/getWorkerDetails";
@@ -12,11 +14,19 @@ import { CountdownAdModal } from "@/src/app/(front)/ADS/Adsterra/CountdownAdModa
 
 interface IWorkerPageProps {
   params: Promise<{ workerId: string }>;
+  searchParams: Promise<{ demo?: string }>;
 }
 
-const WorkerPage = async ({ params }: IWorkerPageProps) => {
+const WorkerPage = async ({ params, searchParams }: IWorkerPageProps) => {
+  const session = await auth();
+
+  const { demo } = await searchParams;
+  const isDemo = demo?.toString().toLowerCase() === "true";
+
+  if (!isDemo && (!session || !session.user)) return <Unauthorized />;
+
   const { workerId } = await params;
-  const { worker } = await getWorkerDetails(workerId);
+  const { worker } = await getWorkerDetails(workerId, isDemo);
 
   return (
     <div className="w-full h-full max-h-full overflow-auto p-2 sm:p-4 md:p-6 flex flex-col gap-6">
@@ -24,10 +34,10 @@ const WorkerPage = async ({ params }: IWorkerPageProps) => {
       <div className="flex flex-col lg:flex-row gap-6 w-full">
         {/* Coluna esquerda */}
         <div className="w-full lg:w-1/2 flex flex-col gap-4">
-          <WorkerInfoCard worker={worker} />
-          <WorkSchedule workerId={workerId} />
-          <RegisterClockModal workerId={workerId} />
-          <RegisterMedicalLeaveModal workerId={workerId} />
+          <WorkerInfoCard worker={worker} isDemo={isDemo} />
+          <WorkSchedule workerId={workerId} isDemo={isDemo} />
+          <RegisterClockModal workerId={workerId} isDemo={isDemo} />
+          <RegisterMedicalLeaveModal workerId={workerId} isDemo={isDemo} />
         </div>
 
         {/* Coluna direita */}
@@ -35,11 +45,13 @@ const WorkerPage = async ({ params }: IWorkerPageProps) => {
           <CalendarInput
             title="Definir data:"
             onSubmitRedirect={`/manager/worker/${workerId}`}
+            isDemo={isDemo}
           />
           <RegistersTable
             workerId={workerId}
             maxRegisters={12}
             overflowAuto={false}
+            isDemo={isDemo}
           />
         </div>
       </div>
