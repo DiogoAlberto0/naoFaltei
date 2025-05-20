7fb57d8cbc9cc9ae06c2947e7030c3599115d0fa
DiogoAlberto0 <dafgo@DiogoAlberto.>
2025-05-19 22:46:16 -0300

    feat: create component `MonetagInterstitial`

---
+++ src/app/(front)/ADS/Monetag/Interstitial.tsx
@@ -0,0 +1,10 @@
+import Script from "next/script";
+
+export const MonetagInterstitial = () => {
+  return (
+    <>
+      <Script src="https://groleegni.net/401/9352217" />
+      {/* <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('groleegni.net',9352217,document.createElement('script'))</script> */}
+    </>
+  );
+};
